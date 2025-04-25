8811988026cd0ee2adb7e055053bf1bc4f0be185
kaaek <khalil.el.kaaki@gmail.com>
2025-04-25 23:16:05 +0300

    Update sp_mul.v

--- Datapath Design.srcs/sources_1/new/sp_mul.v
+++ Datapath Design.srcs/sources_1/new/sp_mul.v
@@ -38,27 +38,39 @@ module sp_mul(
     sp_class aClass(a, aSnan, aQnan, aInfinity, aZero, aSubnormal, aNormal);
     sp_class bClass(b, bSnan, bQnan, bInfinity, bZero, bSubnormal, bNormal);
     
-    reg [31:0] productTemp;
-    reg snan, qnan, infinity, zero, subnormal, normal;
+    reg [31:0] pTemp;
+    reg snan, qnan, infinity, zero, subnormal, normal, pSign;
     
     always @(*)
     begin
-        productTemp = {1'b0, {8{1'b1}}, 1'b0, {22{1'b1}}}; // initialize to sNaN.
+        // IEEE 754-2019: "when neither the inputs nor result are NaN, the sign of the product ... is the exclusive OR of the operads' signs."
+        pSign = a[31] ^ b[31];
+        pTemp = {1'b0, {8{1'b1}}, 1'b0, {22{1'b1}}}; // initialize to sNaN.
         {snan, qnan, infinity, zero, subnormal, normal} = 6'b000000; // initialize to zero. By the end of the implementation, 1 of the flags must necessarily be set, no more and no less.
         if ((aSnan | bSnan) == 1'b1) 
             begin
             // a or b is an sNan, set the product to that value
-                productTemp = aSnan == 1'b1 ? a : b;
+                pTemp = aSnan == 1'b1 ? a : b;
                 snan = 1;
             end
         else if ((aQnan | bQnan) == 1'b1)
         // In this implementation, qNans and sNans are passed the same way, even though the sNaN should raise an exception. Checking both types is separated for the purpose of making it
         // easy for such an integration to occur.
             begin
-                productTemp = aQnan == 1'b1 ? a : b;
+                pTemp = aQnan == 1'b1 ? a : b;
                 qnan = 1;
             end
-        //
+        else if ((aInfinity | bInfinity) == 1'b1)
+            begin
+                qnan = aZero | bZero; // inf * zero = NaN or infinity depending on the value of bit 22
+                infinity = ~qnan;
+                pTemp = {pSign, {8{1'b1}}, qnan, {22{1'b0}}};
+            end
+        else if ((aZero | bZero) == 1'b1)
+            begin
+                pTemp = {pSign, {31{1'b0}}}; // +/- zero
+                zero = 1;
+            end
     end
     
     
