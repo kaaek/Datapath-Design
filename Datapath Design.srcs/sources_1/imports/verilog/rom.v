a9290c83dfeb4e650782365db38d1076c1d81d11
Tarek Mourad <tarekakram11@gmail.com>
2025-04-27 22:55:35 +0300

    Update rom.v

--- Datapath Design.srcs/sources_1/imports/verilog/rom.v
+++ Datapath Design.srcs/sources_1/imports/verilog/rom.v
@@ -178,101 +178,99 @@ module rom (
         rom_array[118] = 8'h00;
         rom_array[119] = 8'h00;
 
-        // Your floating-point test program starts at offset 120 (0x78)
-        // lui x1, 0x10000
+        // 0x100000B7 (lui x1, 0x10000)
         rom_array[120] = 8'hB7;
         rom_array[121] = 8'h00;
         rom_array[122] = 8'h00;
         rom_array[123] = 8'h10;
-
-        // lui x2, 0x3f80
+    
+        // 0x03F80137 (lui x2, 0x3F800)
         rom_array[124] = 8'h37;
-        rom_array[125] = 8'h80;
-        rom_array[126] = 8'h3F;
-        rom_array[127] = 8'h00;
-        
-        // addi x2, x2, 0
+        rom_array[125] = 8'h01;
+        rom_array[126] = 8'hF8;
+        rom_array[127] = 8'h03;
+    
+        // 0x00010113 (addi x2, x2, 0)
         rom_array[128] = 8'h13;
         rom_array[129] = 8'h01;
         rom_array[130] = 8'h01;
         rom_array[131] = 8'h00;
-        
-        // sw x2, 0(x1)
+    
+        // 0x0020A023 (sw x2, 0(x1))
         rom_array[132] = 8'h23;
         rom_array[133] = 8'hA0;
         rom_array[134] = 8'h20;
         rom_array[135] = 8'h00;
-
-        // lui x2, 0x40e0
+    
+        // 0x040E0137 (lui x2, 0x40E00)
         rom_array[136] = 8'h37;
-        rom_array[137] = 8'h00;
-        rom_array[138] = 8'hE0;
-        rom_array[139] = 8'h40;
-        
-        // addi x2, x2, 0
+        rom_array[137] = 8'h01;
+        rom_array[138] = 8'h0E;
+        rom_array[139] = 8'h04;
+    
+        // 0x00010113 (addi x2, x2, 0)
         rom_array[140] = 8'h13;
         rom_array[141] = 8'h01;
         rom_array[142] = 8'h01;
         rom_array[143] = 8'h00;
-        
-        // sw x2, 4(x1)
+    
+        // 0x0020A223 (sw x2, 4(x1))
         rom_array[144] = 8'h23;
         rom_array[145] = 8'hA2;
         rom_array[146] = 8'h20;
         rom_array[147] = 8'h00;
-        
-        // flw f1, 0(x1)
-        rom_array[148] = 8'h07;
+    
+        // 0x0000A087 (flw f1, 0(x1))
+        rom_array[148] = 8'h87;
         rom_array[149] = 8'hA0;
         rom_array[150] = 8'h00;
         rom_array[151] = 8'h00;
-        
-        // flw f2, 4(x1)
+    
+        // 0x0040A107 (flw f2, 4(x1))
         rom_array[152] = 8'h07;
         rom_array[153] = 8'hA1;
         rom_array[154] = 8'h40;
         rom_array[155] = 8'h00;
-        
-        // fadd.s f3, f1, f2
-        rom_array[156] = 8'h53;
+    
+        // 0x002081D3 (fadd.s f3, f1, f2))
+        rom_array[156] = 8'hD3;
         rom_array[157] = 8'h81;
         rom_array[158] = 8'h20;
-        rom_array[159] = 8'h00;
-        
-        // fsub.s f4, f2, f1
+        rom_array[159] = 8'h02;
+    
+        // 0x08110253 (fsub.s f4, f2, f1))
         rom_array[160] = 8'h53;
         rom_array[161] = 8'h02;
         rom_array[162] = 8'h11;
         rom_array[163] = 8'h08;
-        
-        // fmul.s f5, f1, f2
-        rom_array[164] = 8'h53;
-        rom_array[165] = 8'h12;
+    
+        // 0x102082D3 (fmul.s f5, f1, f2))
+        rom_array[164] = 8'hD3;
+        rom_array[165] = 8'h82;
         rom_array[166] = 8'h20;
         rom_array[167] = 8'h10;
-        
-        // fsw f3, 8(x1)
+    
+        // 0x0030A427 (fsw f3, 8(x1))
         rom_array[168] = 8'h27;
         rom_array[169] = 8'hA4;
         rom_array[170] = 8'h30;
         rom_array[171] = 8'h00;
-
-        // fsw f4, 12(x1)
+    
+        // 0x0040A627 (fsw f4, 12(x1))
         rom_array[172] = 8'h27;
         rom_array[173] = 8'hA6;
         rom_array[174] = 8'h40;
         rom_array[175] = 8'h00;
-
-        // fsw f5, 16(x1)
+    
+        // 0x0050A827 (fsw f5, 16(x1))
         rom_array[176] = 8'h27;
         rom_array[177] = 8'hA8;
         rom_array[178] = 8'h50;
         rom_array[179] = 8'h00;
-        
-        // Zero out remaining memory
-        for (i = 180; i < 2**(`ADDRLEN); i = i + 1) 
-        begin
-            rom_array[i] = 8'h00;
-        end
+    
+    for (i = 180; i < 2**(`ADDRLEN); i = i + 1) 
+    begin
+      rom_array[i] = 8'h00;
     end
+  end
 endmodule
